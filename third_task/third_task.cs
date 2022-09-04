0a620667b275934ccdf7c95d1691f555f91bf7bd
LeipreachanTeo <pokiboom@mail.ru>
2022-09-04 16:46:02 +0700

    third task

---
+++ third_task/third_task.cs
@@ -0,0 +1,23 @@
+﻿int N = new Random().Next(5,10);
+int[] array = new int[N];
+int difference = 0;
+
+for(int i =0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0 ,100);
+Console.WriteLine(array[i]);
+}
+int max = array[0];
+int min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }    
+    else if( array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine(max - min);
