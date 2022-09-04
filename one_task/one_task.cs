dce8830c015dec2ccd10270870eaa0a9536f59d4
LeipreachanTeo <pokiboom@mail.ru>
2022-09-04 16:15:11 +0700

    first task

---
+++ one_task/one_task.cs
@@ -0,0 +1,12 @@
+﻿int N = new Random().Next(5,10);
+int[] array = new int[N];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (array[i] % 2 ==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
