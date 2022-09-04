ba91f0453956f7d530d3f07c9a4c5b5417aaccad
LeipreachanTeo <pokiboom@mail.ru>
2022-09-04 16:26:08 +0700

    second task

---
+++ second_task/second_task.cs
@@ -0,0 +1,13 @@
+﻿int N = new Random().Next(5,10);
+int[] array = new int[N];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0 ,99999);
+    if (array[i] % 2 ==0 && i % 2 != 0)
+    {
+        sum += array[i];
+    }   
+}
+Console.WriteLine(sum);
